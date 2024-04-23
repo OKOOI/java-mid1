50f82069fa2f4c44fcd68d0dcd8241edee0bc4f5
OKOOI <jyslovly1993@gmail.com>
2024-04-23 11:57:39 +0900

    Object toString equals

---
+++ src/lang/object/ObjectMain.java
@@ -0,0 +1,17 @@
+package lang.object;
+
+public class ObjectMain {
+
+	public static void main(String[] args) {
+
+		Child child = new Child();
+		child.childMethod();
+		child.parentMethod();
+		
+		// toStrong()은 Object 클래스의 매서드
+		String string = child.toString();
+		System.out.println(string);
+
+	}
+
+}
---
+++ src/lang/object/tostirng/Car.java
@@ -0,0 +1,11 @@
+package lang.object.tostirng;
+
+public class Car {
+	
+	private String CarName;
+	
+	public Car(String carName) {
+		this.CarName = carName;
+	}
+
+}
